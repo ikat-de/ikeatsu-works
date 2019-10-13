cd9e3e2819abd9ad7472f626a3a802133a470894
Ikeda Atsushi <a.ikd05@hotmail.co.jp>
2019-10-13 05:10:57 +0000

    :horse: Fastely の設定を編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
+  config.assets.digest = true  
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'ikeatsu-works-herokuapp-com.global.ssl.fastly.net'
   config.static_cache_control = 'public, s-maxage=2592000, max-age=86400'
