84c3b8320d217ab035df83253d0f154e80adcc41
Ikeda Atsushi <a.ikd05@hotmail.co.jp>
2019-10-19 12:46:45 +0000

    :sparkles: メタタグを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title:       "IKE-ATSU WORKS",
+      description: "池田篤士のポートフォリオサイト",
+      keywords:    "デザイン,webサイト,デザインエンジニア",
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('apple-touch-icon-180x180.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      noindex: ! Rails.env.production?,
+	  canonical: request.original_url,
+      charset: "UTF-8",
+      og: {
+        title: :title,
+        description: :description,
+        type: "website",
+        url: request.original_url,
+        image: image_url("https://ikeatsu-works.s3-ap-northeast-1.amazonaws.com/ikeatsu-works-twitter-card.jpg"),
+        site_name: "IKE-ATSU WORKS",
+        locale: "ja_JP"
+      },
+      twitter: {
+        site: '@ikat_de',
+        card: 'summary_large_image',
+        image: image_url("https://ikeatsu-works.s3-ap-northeast-1.amazonaws.com/ikeatsu-works-twitter-card.jpg")
+	  },
+    }
+  end 
 end
